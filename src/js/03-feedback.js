02867a0fe1b8219a879baeb69821dd50459fa1b8
Protsiuk-Prits Dmytro <78557776+ProtsyukPrits@users.noreply.github.com>
2022-08-07 10:34:52 +0200

    finished hm-8

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+  feedbackForm: document.querySelector('.feedback-form'),
+  feedbackFormEmail: document.querySelector('input'),
+  feedbackFormMessage: document.querySelector('textarea'),
+};
+
+refs.feedbackForm.addEventListener('submit', handleOnForm);
+refs.feedbackFormMessage.addEventListener(
+  'input',
+  throttle(handleOnMessage, 500)
+);
+refs.feedbackForm.addEventListener('input', e => {
+  formData[e.target.name] = e.target.value;
+  console.log(formData);
+});
+
+const formData = {};
+populateTextarea();
+
+function handleOnForm(e) {
+  e.preventDefault();
+  e.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+}
+
+function handleOnMessage(e) {
+  const value = e.target.value;
+  localStorage.setItem('feedback-form-state', value);
+}
+
+function populateTextarea() {
+  const savedMessage = localStorage.getItem('feedback-form-state');
+
+  if (savedMessage) {
+    refs.feedbackFormMessage.value = savedMessage;
+  }
+}
